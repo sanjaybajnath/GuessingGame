6cc19baf284763399b963ba0ce4d789b2f43cc31
BT_1N3_22@BKTECH.LOCAL <BT_1N3_22@BKTECH.LOCAL>
2018-10-10 14:48:16 -0400

    Initial commit

---
+++ src/GuessingGame.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class GuessingGame {
+    public static void main (String args[]){
+        Scanner input = new Scanner(System.in);
+        int guesses = 0;
+        System.out.println("Choose a difficulty: ");
+        System.out.println("Type 1 for numbers 1 - 10");
+        System.out.println("Type 2 for numbers 1 - 100");
+        System.out.println("Type 3 for numbers 1 - 1000");
+        System.out.println("Type 4 for numbers 1 - 10000");
+
+    }
+}
