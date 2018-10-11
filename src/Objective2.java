11df04448abb3fe1872e153e6d14a6eb9645c4f2
sanjaybajnath <sbajnath6151@bths.edu>
2018-10-11 14:48:18 -0400

    Started objective 2

---
+++ src/Objective2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Objective2 {
+    public static void main (String [] args){
+        int num = 50;
+        Scanner input = new Scanner (System.in);
+        System.out.println("Is your number ");
+        while (!input.nextLine().equals("correct")){
+            if (input.nextLine().equals("lower")){
+
+            }
+        }
+    }
+}
