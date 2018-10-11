d068bdbc5b80f3cca333ad07f81415f88e5f58e3
sanjaybajnath <sbajnath6151@bths.edu>
2018-10-11 18:55:03 -0400

    Objective 2 completed

--- src/Objective2.java
+++ src/Objective2.java
@@ -3,12 +3,25 @@ import java.util.Scanner;
 public class Objective2 {
     public static void main (String [] args){
         int num = 50;
+        int min = 0;
+        int max = 100;
+        int guesses = 1;
         Scanner input = new Scanner (System.in);
-        System.out.println("Is your number ");
-        while (!input.nextLine().equals("correct")){
-            if (input.nextLine().equals("lower")){
-
+        System.out.println("Is your number "+ num+"?");
+        String str = input.nextLine();
+        while (!str.equals("correct")){
+            if (str.equals("lower")){
+                max = num-1;
             }
+            if (str.equals("higher")){
+                min = num+1;
+            }
+            guesses ++;
+            double x = Math.random();
+            num = (int)(x*(max-min)+min);
+            System.out.println("Is your number "+num+"?");
+            str = input.nextLine();
         }
+        System.out.println("This program correctly guessed your number, "+num+", after "+guesses+" attempts!");
     }
 }
